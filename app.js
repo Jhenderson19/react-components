0b76d011d181e604aa5023c4bb0108192a450700
James Henderson <jhende1995@gmail.com>
2020-11-30 23:17:44 -0500

    Complete creating and rendering nested React Components

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // TODO
+var GroceryList = (props) => (
+  <ul>
+    {<Cucumbers />}
+    {<Apricots />}
+  </ul>
+);
+var Cucumbers = ( ) => (
+  <li>Cucumbers</li>
+)
+var Apricots = ( ) => (
+  <li>Apricots</li>
+)
+
+ReactDOM.render(<GroceryList item0={"Cucumbers"} item1= {"Apricots"}/>, document.getElementById("app"))
